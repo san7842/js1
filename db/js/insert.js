4ae41b089b5d1e020c694d2454dcb42fcc73d59e
san7842 <sandeepjjjjj536@gmail.com>
2025-11-25 20:27:00 +0530

    add

---
+++ db/js/insert.js
@@ -0,0 +1,23 @@
+document.getElementById("btn1").addEventListener("click",dataSave);
+
+async function dataSave(){
+    let pname=document.getElementById("prname").value;
+    let pprice=document.getElementById("prprice").value;
+    let pcity=document.getElementById("prcity").value;
+    let pcontact=document.getElementById("prcontact").value;
+
+    let api="http://localhost:3000/products";
+    const response=await fetch(api,{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            "name":pname,
+            "price":pprice,
+            "city":pcity,
+           "contact":pcontact 
+        })
+        } )
+        alert("Data succefully save!!1")
+}
